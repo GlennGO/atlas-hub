a45c77cc1a7cdfd312145f8f040b3310ab52d52b
GlennGO Hermes <hermes@glenngo.com>
2026-07-19 17:10:05 +0000

    fix: enable standalone output for Docker build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
+  output: "standalone",
   webpack: (config) => {
     config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
